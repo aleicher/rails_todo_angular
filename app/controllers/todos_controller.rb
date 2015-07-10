d1ce5c351b503b1821ae057ce9875f715da3b27c
Andreas Leicher <andreas.leicher@mapcase.de>
2015-07-10 17:07:15 +0200

    todos controller responds with json data
    - rails 4 requires the responder gem
    - rendering json requires to define that now view is rendered, but the
      controller responds_with the object itself

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,6 @@
 class TodosController < ApplicationController
+  respond_to :json
+  def index
+    respond_with Todo.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :todos
+  scope :api do
+    resources :todos, defaults: {format: :json}
+  end
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
